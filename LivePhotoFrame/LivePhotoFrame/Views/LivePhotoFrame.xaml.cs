d3c891b5e6b5f6149cbab5a72c5ef143f99655c8
Daniel Salunga <dsalunga@live.com>
2018-01-01 17:32:36 +0800

    Initial Project code

---
+++ LivePhotoFrame/LivePhotoFrame/Views/LivePhotoFrame.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.ViewManagement;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LivePhotoFrame.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LivePhotoFrame : ContentPage
+	{
+		public LivePhotoFrame ()
+		{
+			InitializeComponent ();
+
+            ApplicationView.GetForCurrentView().TryEnterFullScreenMode();
+        }
+
+        async void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+        {
+            ApplicationView.GetForCurrentView().ExitFullScreenMode();
+            await Navigation.PopModalAsync();
+        }
+    }
+}
