96830ae25798da6f269036ca6d43ba96244aab2d
Daniel Salunga <dsalunga@live.com>
2018-01-08 00:16:44 +0800

    Add WebApp for REST services and management web interface

---
+++ LivePhotoFrame.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LivePhotoFrame.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
